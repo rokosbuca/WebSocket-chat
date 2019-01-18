839ab1e5112769b27a0adf70c7c2648c8a6635b2
rsbuca <rsb.git@coder.hr>
2019-01-18 21:50:19 +0100

    Added redis storage for saving chatroom-user relationships.

---
+++ server/cluster/chatroom-user-storage.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author rsb
+ */
+'use strict';
+
+// cluster client
+const clusterClient = require('./cluster-client').getClient();
+
+const connectChatroomUser = (chatroom, user) => {
+
+}
+
+module.exports = {
+    connectChatroomUser
+}
