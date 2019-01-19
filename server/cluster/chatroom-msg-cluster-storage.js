0ad6e9e79e0256520952297c778b8bb8873349f7
rsbuca <rsb.git@coder.hr>
2019-01-20 00:42:11 +0100

    Added chatroomInitMessage which initializes the chatroom-message model on redis.

--- server/cluster/chatroom-msg-cluster-storage.js
+++ server/cluster/chatroom-msg-cluster-storage.js
@@ -7,10 +7,34 @@
 // cluster client
 const clusterClient = require('./cluster-client').getClient();
 
+const chatroomInitMessage = (chatroom, timestamp, user) => {
+    // add two messages when a chatroom is first created
+    // 1) User 'username' created chatroom 'chatroom' on 'timestamp'
+    // 2) User 'username' connected to chatroom 'chatroom' on 'timestamp'
+
+    const initMessage = 'User ' + user + ' created chatroom ' + chatroom + ' on ' + timestamp;
+    
+    return new Promise((resolve, reject) => {
+        clusterClient._rpush(chatroom, initMessage)
+        .then(() => {
+            const adminConnectedMessage = 'User ' + user + ' connected to chatroom ' + chatroom + ' on ' + new Date().toLocaleString();
+    
+            clusterClient._rpush(chatroom, adminConnectedMessage)
+            .then((listLenght) => {
+                resolve(listLenght)
+            })
+        })
+        .catch((error) => {
+            reject(error);
+        });
+    });
+}
+
 const newMessage = (chatroom, timestamp, user, message) => {
 
 }
 
 module.exports = {
+    chatroomInitMessage,
     newMessage
 }
