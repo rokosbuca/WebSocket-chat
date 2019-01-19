735390ff26d6916aaa160b96c1e7dd3839fe4c02
rsbuca <rsb.git@coder.hr>
2019-01-19 19:28:09 +0100

    Added some doc to the chatroom controller.

--- server/controller/chatroom-controller.js
+++ server/controller/chatroom-controller.js
@@ -14,6 +14,12 @@ const mapping = '/rooms';
 const chatroomStorage = require('../cluster/chatroom-cluster-storage');
 
 const getChatRoomList = (req, res) => {
+    // return a list of strings representing chat rooms of this format:
+    // "chatroomName;nUsers;nMessages;chatroomAge;hasPassword;adminUsername"
+    // sort list by chatroom's age
+
+
+    
     return res.status(200).json({
         'message': 'GET ' + mapping,
         'req.query': req.query,
