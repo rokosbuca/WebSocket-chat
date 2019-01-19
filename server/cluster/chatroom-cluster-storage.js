d97e6c7310cf2cc99b4fff71eeb6bec4d6708dff
rsbuca <rsb.git@coder.hr>
2019-01-19 19:28:30 +0100

    Added getChatRooms method.

--- server/cluster/chatroom-cluster-storage.js
+++ server/cluster/chatroom-cluster-storage.js
@@ -8,7 +8,8 @@
  *  - created by
  * 
  * Model is saved to a hash map with the map name of 'chatrooms:info'
- * Hash map's keys are chatrooms' names
+ * Hash map's keys are chatrooms' names. Chatroom name is still saved to
+ * info string redundantly.
  * 
  * Model is initialized at chatroom creation time and cannot be changed.
  * 
@@ -34,7 +35,7 @@ const map = 'chatrooms:info';
  * @param {String} createdAtTime - Defines a time at which the chatroom was created
  * @param {String} createdByUser - User who created this chatroom
  * 
- * @returns {String} Returns the result of the redis client's hset method
+ * @returns {String} Forwards the result of the redis client's hset method
  */
 const createChatRoom = (chatroomName, chatroomPassword, createdAtTime, createdByUser) => {
     
@@ -46,7 +47,39 @@ const createChatRoom = (chatroomName, chatroomPassword, createdAtTime, createdBy
  * @returns {Array} An array of chatroom info objects. Attributes: name, password, createdAt, createdBy.
  */
 const getChatRooms = () => {
+    const chatrooms = [];
 
+    clusterClient._hkeys(map)
+    .then((chatroomNames) => {
+        const getPromises = [];
+
+        chatroomNames.forEach((chatroomName) => {
+            getPromises.push(
+                clusterClient._hget(map, chatroomName)
+            );
+        });
+
+        Promise.all(getPromises)
+        .then((chatroomInfos) => {
+            chatroomInfos.forEach((chatroomInfo) => {
+                const chatroom = {};
+                chatroom.name = chatroomInfo.name;
+                chatroom.password = (chatroomInfo.password ? chatroomInfo.password : "");
+                chatroom.createdAt = chatroomInfo.createdAt;
+                chatroom.createdBy = chatroomInfo.createdBy;
+
+                chatrooms.push(chatroom);
+            });
+
+            return chatrooms;
+        })
+        .catch((error) => {
+            throw error;
+        });
+    })
+    .catch((error) => {
+        throw error;
+    });
 }
 
 /**
@@ -105,9 +138,6 @@ const checkPassword = (chatroom, password) => {
  * @returns {boolean} True if the user has admin privileges, false otherwise. 
  */
 const isUserAdmin = (chatroom, user) => {
-    // checks if user has admin privileges
-    // only the user who created a chatroom has admin privileges
-    // admin can delete a chatroom
 
 }
 
