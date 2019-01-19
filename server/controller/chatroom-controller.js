6c19856db5162cdbe65b298cb2a54bf6086e752f
rsbuca <rsb.git@coder.hr>
2019-01-19 20:35:54 +0100

    Added createNewChatroom method. Linked it to POST api/chatrooms. Tested with Postman.

--- server/controller/chatroom-controller.js
+++ server/controller/chatroom-controller.js
@@ -44,9 +44,37 @@ const getChatRoomList = (req, res) => {
     });
 }
 
+const createNewChatroom = (req, res) => {
+    const chatroom = req.body.chatroom.chatroom;
+    const password = req.body.chatroom.password;
+    const createdAt = req.body.chatroom.createdAt;
+    const createdBy = req.body.chatroom.createdBy;
+
+    chatroomStorage.createChatRoom(chatroom, password, createdAt, createdBy)
+    .then(() => {
+        return res.status(200).json({
+            'chatroom': {
+                'chatroom': chatroom,
+                'password': password,
+                'createdAt': createdAt,
+                'createdBy': createdBy
+            }
+        });
+    })
+    .catch((error) => {
+        console.log('Error while accessing POST api/chatrooms. Error message:', error);
+        return res.status(500).send('Unexpected server error while creating chatroom ' + chatroom + '.');
+    })
+}
+
 router.get(mapping,
     getChatRoomList
 );
+
+router.post(mapping,
+    createNewChatroom
+);
+
 
 module.exports = {
     router
