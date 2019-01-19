2a775b7a21f71b2066cc8b572837b8ba1239f055
rsbuca <rsb.git@coder.hr>
2019-01-19 21:17:48 +0100

    Changed get request for chatrooms. Added 'loading' attribute. Added create new chatroom button.

--- webapp/src/components/Homepage.js
+++ webapp/src/components/Homepage.js
@@ -1,38 +1,83 @@
 import React, { Component } from 'react';
-import { List } from 'antd';
+import { Button, List } from 'antd';
 import axios from 'axios';
 
 import ChatRoomInfo from './ChatRoomInfo';
+
+const urlChatRooms = 'http://localhost:3001/api/chatrooms';
 
 class Homepage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            chatRoomList: []
+            loading: true,
+            chatroomList: []
         }
     }
 
     componentDidMount = () => {
-        axios.get('http://localhost:3001/api/chatrooms')
+        this.getChatrooms();
+    }
+
+    getChatrooms = () => {
+        axios.get(urlChatRooms)
         .then((res) => {
-            this.setState({ chatRoomList: res.data.chatrooms });
+            this.setState({
+                loading: false,
+                chatroomList: res.data.chatrooms
+            });
         })
         .catch((responseObject) => {
             console.log('Caught an error - responseObject:', responseObject);
+            this.setState({
+                loading: false,
+                chatroomList: []
+            });
         });
+    }
+
+    createNewChatroom = () => {
+        const chatroom = {
+            chatroom: "ch2",
+            password: "",
+            createdAt: "21:07:13",
+            createdBy: "mega admin"
+        }
+        axios.post(urlChatRooms, { chatroom })
+        .then((res) => {
+            console.log(res);
+            this.addChatroomToList('new chatroom'/*res.data.chatroom.chatroom*/);
+        })
+        .catch((responseObject) => {
+            console.log('Error. Response object:', responseObject);
+        })
+    }
+
+    addChatroomToList = (chatroom) => {
+        const newChatroomList = this.state.chatroomList;
+        newChatroomList.push(chatroom);
+
+        this.setState({ chatroomList: newChatroomList });
     }
 
     render() { 
         return (
             <div style={{ marginLeft: 32 }}>
-                <h1>ChatRoom Homepage</h1>
+                <h1>Chatroom Homepage</h1>
                 <br /><br />
-                <h3>List of ChatRooms:</h3>
+                <h3>List of Chat Rooms:</h3>
                 <List
                 size="default"
-                    header={ <div>Header</div> }
-                    bordered
-                    dataSource={ this.state.chatRoomList }
+                    title="List of Chat Rooms:"
+                    header={
+                        <Button onClick={ this.createNewChatroom }
+                        >
+                            Create New Chat Room
+                        </Button>
+                    }
+                    bordered={ true }
+                    loading={ this.state.loading }
+                    dataSource={ this.state.chatroomList }
                     renderItem={ item => (<List.Item>{item}</List.Item>) }
                 />
             </div>
