b7c95cd6ebe8f7733e0419ca3d4a8afceb18caa1
rsbuca <rsb.git@coder.hr>
2019-01-20 03:07:09 +0100

    Changed chatroom-user relationship data shape.

--- server/cluster/chatroom-user-cluster-storage.js
+++ server/cluster/chatroom-user-cluster-storage.js
@@ -7,11 +7,26 @@
 // cluster client
 const clusterClient = require('./cluster-client').getClient();
 
+// chatroom hash map's name
+const map = 'chatrooms:users';
+
+// contains data as follows
+//      "chatroom:users" "chatroom1" "user1;user2;user3;user4"
+//      "chatroom:users" "chatroom2" "user1;user2;user3;user4"
+//      "chatroom:users" "chatroom3" "user1;user2;user3;user4"
+
 const connectChatroomUser = (chatroom, user) => {
     return new Promise((resolve, reject) => {
-        clusterClient._hset(chatroom, user, 'active')
-        .then(() => {
-            resolve();
+        clusterClient._hget(map, chatroom)
+        .then((users) => {
+            const newUserList = (users ? users + ';' + user : user);
+            clusterClient._hset(map, chatroom, newUserList)
+            .then(() => {
+                resolve();
+            })
+            .catch((error) => {
+                reject(error);
+            });
         })
         .catch((error) => {
             reject(error);
@@ -22,7 +37,7 @@ const connectChatroomUser = (chatroom, user) => {
 const disconnectChatroomUser = (chatroom, user, isAdmin) => {
     return new Promise((resolve, reject) => {
         if (isAdmin) {
-            clusterClient._hdelall(chatroom)
+            clusterClient._hdel(map, chatroom)
             .then(() => {
                 resolve();
             })
@@ -30,16 +45,79 @@ const disconnectChatroomUser = (chatroom, user, isAdmin) => {
                 reject(error);
             });
         } else {
-            clusterClient._hdel(chatroom, user);
+            clusterClient._hget(map, chatroom)
+            .then((users) => {
+                const userList = users.split(';');
+                const remainingUsersList = [];
+
+                userList.forEach((userInList) => {
+                    if (user !== userInList) {
+                        remainingUsersList.push(userInList);
+                    }
+                });
+
+                let remainingUsersString = '';
+                for (let i = 0; i < remainingUsersList.length; i++) {
+                    if (i !== 0) {
+                        remainingUsersString += ';';
+                    }
+
+                    remainingUsersString += remainingUsersList[i];
+                }
+                clusterClient._hset(map, chatroom, remainingUsersString)
+                .then(() => {
+                    resolve();
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+            })
+            .catch((error) => {
+                reject(error);
+            });
         }
     });
 }
 
 const countUsers = (chatroom) => {
     return new Promise((resolve, reject) => {
-        clusterClient._hkeys(chatroom)
-        .then((keys) => {
-            resolve(keys.length);
+        clusterClient._hget(chatroom)
+        .then((users) => {
+            resolve(users.split(';').length);
+        })
+        .catch((error) => {
+            reject(error);
+        });
+    });
+}
+
+const countUsersForAllChatrooms = () => {
+    return new Promise((resolve, reject) => {
+        const chatroomNames = [];
+        const countUsersPromises = [];
+
+        clusterClient._hkeys(map)
+        .then((chatrooms) => {
+            chatrooms.forEach((chatroom) => {
+                chatroomNames.push(chatroom);
+                countUsersPromises.push(countUsers(chatroom));
+            });
+
+            Promise.all(countUsersPromises)
+            .then((counts) => {
+                const chatroomUserCount = [];
+                for (let i = 0; i < counts.length; i++) {
+                    chatroomUserCount.push({
+                        chatroom: chatroomNames[i],
+                        userCount: counts[i]
+                    });
+                }
+
+                resolve(chatroomUserCount);
+            })
+            .catch((error) => {
+                reject(error);
+            });
         })
         .catch((error) => {
             reject(error);
@@ -49,13 +127,15 @@ const countUsers = (chatroom) => {
 
 const userExists = (chatroom, user) => {
     return new Promise((resolve, reject) => {
-        clusterClient._hget(chatroom, user)
-        .then((result) => {
-            if (result === 'active') {
-                resolve(true);
-            } else {
-                resolve(false);
-            }
+        clusterClient._hget(map, chatroom)
+        .then((users) => {
+            users.forEach((existingUser) => {
+                if (user === existingUser) {
+                    resolve(true);
+                }
+            });
+
+            resolve(false);
         })
         .catch((error) => {
             reject(error);
@@ -67,5 +147,6 @@ module.exports = {
     connectChatroomUser,
     disconnectChatroomUser,
     countUsers,
+    countUsersForAllChatrooms,
     userExists
 }
