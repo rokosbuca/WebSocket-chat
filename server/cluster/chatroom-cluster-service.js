6e4a1c20a7276df8d0f9df59469cb08c1f51dcc2
rsbuca <rsb.git@coder.hr>
2019-01-20 03:21:16 +0100

    Added getChatroomsList method to be used by controllers.

--- server/cluster/chatroom-cluster-service.js
+++ server/cluster/chatroom-cluster-service.js
@@ -4,7 +4,75 @@
  */
 'use strict';
 
+// cluster storage modules
+const chatroomInfoStorage = require('./chatroom-cluster-storage');
+const chatroomUserStorage = require('./chatroom-user-cluster-storage');
+const chatroomMsgStorage = require('./chatroom-msg-cluster-storage');
+
+const getChatroomsList = () => {
+    // return chatroom as an object containing:
+    //      - chatroom
+    //      - password
+    //      - createdAt
+    //      - createdBy
+    //      - nUsers
+    //      - nMessages
+
+    const chatroomsList = [];
+
+    return new Promise((resolve, reject) => {
+        chatroomInfoStorage.getChatRooms()
+        .then((chatroomsInfos) => {
+            chatroomsInfos.forEach((chatroomInfo) => {
+                chatroomsList.push(
+                    {
+                        chatroom: chatroomInfo.chatroom,
+                        password: chatroomInfo.password,
+                        createdAt: chatroomInfo.createdAt,
+                        createdBy: chatroomInfo.createdBy
+                    }
+                );
+            });
+
+            // get all chatroom-user counts
+            // it is in format {ch1: 20, ch2: 4, ch3: 1, ...}
+            chatroomUserStorage.countUsersForAllChatrooms()
+            .then((chatroomUserCounts) =>{
+                chatroomsList.forEach((chatroomObject) => {
+                    chatroomObject.nUsers = chatroomUserCounts.chatroom;
+                });
+
+                // get all chatroom-messages counts
+                const chatroomMessagesCountPromises = [];
+                chatroomsList.forEach((chatroomObject) => {
+                    chatroomMessagesCountPromises.push(
+                        chatroomMsgStorage.countMessages(chatroomObject.chatroom)
+                        .then((count) => {
+                            chatroomObject.nMessages = count;
+                        })
+                        .catch((error) => {
+                            reject(error);
+                        })
+                    );
+                });
+                Promise.all(chatroomMessagesCountPromises)
+                    .then(() => {
+                        resolve(chatroomsList);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        })
+        .catch((error) => {
+            reject(error);
+        });
+    });
+}
 
 module.exports = {
-    
+    getChatroomsList
 }
